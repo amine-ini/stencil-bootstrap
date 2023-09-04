36f6fcd4765a4d248b23084ad28c85a6a4df4bf5
amine <a.bougassaa@abo-s.com>
2023-09-04 12:18:28 +0200

    improve card stories

--- src/components/ini-card/ini-card.stories.ts
+++ src/components/ini-card/ini-card.stories.ts
@@ -12,6 +12,6 @@ export const Overview = {
     args: {
         headerTitle: 'The header card title',
         children: `<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolorum esse est ipsa minus modi necessitatibus non praesentium quasi quod.</p>
-        <ini-button>Button 1</ini-button> <ini-button variant="secondary">Button 2</ini-button>`
+        <ini-button size="small" outline>Button 1</ini-button> <ini-button size="small" outline variant="secondary">Button 2</ini-button>`
     },
 };
